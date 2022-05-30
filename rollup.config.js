869f75c2181595231cbfebe1b13b781177e6d862
Innei <tukon479@gmail.com>
2022-05-31 00:09:46 +0800

    fix: css import

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 // @ts-check
 // import { babel } from '@rollup/plugin-babel'
-import css from 'rollup-plugin-import-css'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import css from 'rollup-plugin-postcss'
 import { terser } from 'rollup-plugin-terser'
 
 import commonjs from '@rollup/plugin-commonjs'
